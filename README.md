b6e0ba9d5791a0f17bfff358a02050419711e50f
DaiAkiba <gaslakiba@gmail.com>
2014-04-23 21:27:53 +0900

    サーバ環境生成構築

--- README.md
+++ README.md
@@ -7,9 +7,9 @@ PHP Enviroment in Asl Local Only
 
 ツール | 取得元 | 確認時のバージョン | メモ
 :------|:-------|:-------------------|:-----
-VirtualBox | https://www.virtualbox.org/wiki/Downloads | 4.2.12 r84980 |
-Vagrant | http://www.vagrantup.com/downloads.html | 1.3.4 |
-Chef Client | http://www.getchef.com/chef/install/ | 11.8.0 |
+VirtualBox | https://www.virtualbox.org/wiki/Downloads | 4.3.10 r93012 |
+Vagrant | http://www.vagrantup.com/downloads.html | 1.5.4 |
+Chef Client | http://www.getchef.com/chef/install/ | 11.12.2 |
 knife-solo | https://github.com/matschaffer/knife-solo | 0.4.0 |
 Git Bash | - | - | Windowsの場合に必要<br>chefを使うときにrsync.exeが必要になります<br>rsync.exeはcygwin経由で持ってきました<br>[cygwinでapt-cygを使う](http://wp.krks.net/2013/09/cygwin%E3%81%AE%E3%82%A4%E3%83%B3%E3%82%B9%E3%83%88%E3%83%BC%E3%83%AB%EF%BC%8664bit%E3%81%A7%E3%81%AEapt-cyg%E3%82%92%E5%88%A9%E7%94%A8%E3%81%A7%E3%81%8D%E3%82%8B%E3%82%88%E3%81%86%E3%81%AB%E3%81%99/)
 
---
+++ chef-repo/site-cookbooks/asl-php-env/recipes/default.rb
@@ -0,0 +1,54 @@
+#
+# Cookbook Name:: asl-php-env
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+#stop iptables
+service "iptables" do
+  action [:stop, :disable]
+end
+
+#install apache php
+%w(apache2 php5 libapache2-mod-php5 php5-mcrypt).each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+#install for MS-SQL connection
+%w(freetds-common freetds-bin unixodbc php5-sybase).each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+#apache2 default directory change
+template "default" do
+  path "/etc/apache2/sites-available/default"
+  source "default.erb"
+  mode 0644
+end
+
+#php enable on apache2
+template "php5.conf" do
+  path "/etc/apache2/sites-available/php5.conf"
+  source "php5.conf.erb"
+  mode 0644
+end
+
+#freetds setting for MS-SQL connection
+template "freetds.conf" do
+  path "/etc/freetds/freetds.conf"
+  source "freetds.conf.erb"
+  mode 0644
+end
+
+#service configuration
+service "apache2" do
+  supports :status => true, :restart => true
+  action [:enable, :restart]
+end
